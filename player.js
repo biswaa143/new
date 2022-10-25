21c2747bfa542af8448b7224b262ad012e9cdf64
biswaa143 <112749931+biswaa143@users.noreply.github.com>
2022-10-25 13:48:02 +0530

    Add a new player.

--- player.js
+++ player.js
@@ -3,6 +3,8 @@ public class Main
 	public static void main(String[] args) {
 	    String firstName = "Virat";
 	    String lastName = "Kohli";
+	    String name = "MS.Dhoni";
 	    System.out.println(firstName + " " + lastName);
+	    System.out.println(name);
 	}
 }
